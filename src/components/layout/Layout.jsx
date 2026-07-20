44db1f9f07505fce7565f4161e9a8abc17586101
bonthapallysankeerth <bonthapallysankeerth@gmail.com>
2026-07-20 13:55:31 +0530

    scroll

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Header from '../header/Header'
 import Footer from '../footer/Footer'
 import BottomNav from './BottomNav'
+import ScrollToTop from './ScrollToTop'
 import Routers from '../routers/Routers'
 import { BrowserRouter as Router } from 'react-router-dom'
 
 function Layout() {
   return (
     <Router>
+      <ScrollToTop />
       <div className="flex min-h-[100dvh] flex-col bg-gray-50">
         <Header />
         <Routers />
